f3cc6ecc5d5d6cd75032969e8e41949068948238
moein <48513001+moeinrouhani@users.noreply.github.com>
2019-05-01 21:51:48 +0430

    Add files via upload

---
+++ assignment7/main.java
@@ -0,0 +1,7 @@
+package assignment7;
+
+public class main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ assignment7/square.java
@@ -0,0 +1,12 @@
+package assignment7;
+import java.util.Scanner;
+
+public class square extends shape {
+
+
+    Scanner sc = new Scanner ( System.in );
+    int side = sc.nextInt ();
+
+
+}
+
